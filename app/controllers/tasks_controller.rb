f22d95633444a436fd4aa19bd8150f53a95ad508
kou.mizukami <kou0830fm@outlook.jp>
2019-09-15 07:52:57 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
     before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
     
     def index
         @tasks = Task.order(id: :desc).page(params[:page]).per(5)
@@ -14,14 +16,15 @@ class TasksController < ApplicationController
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = "Taskが正常に投稿されました！"
-            redirect_to tasks_url(@task)
+            redirect_to("/")
         else
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
             flash.now[:danger] = "Taskが投稿されませんでした！"
-            render :new
+             render 'toppages/index'
         end
     end
     
@@ -45,7 +48,7 @@ class TasksController < ApplicationController
         @task.destroy
         
         flash[:success] = "タスクは正常に削除されました！"
-        redirect_to tasks_url
+        redirect_to("/")
     end
     
     private
@@ -57,4 +60,12 @@ class TasksController < ApplicationController
     def task_params
         params.require(:task).permit(:content, :status)
     end
+    
+    def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to("/")
+      end
+    end
+  
 end
